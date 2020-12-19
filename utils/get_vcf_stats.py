5ad0d956911e6dd961589955f644832d5ec59993
Amir Rubin <amrubin@microsoft.com>
2020-12-19 20:17:33 +0200

    fix outputs for log

--- utils/get_vcf_stats.py
+++ utils/get_vcf_stats.py
@@ -1,41 +1,44 @@
 import subprocess
 import sys
 import os
- 
 
+# params are: input, output folder, prefix(chr name is good, like chr9)
+# example:
+# python get_vcf_stats.py /vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/hgdp_wgs.20190516.full.chr9.vcf.gz /vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/stats/ chr9
 def get_vcf_stats():
     gzvcf_file = sys.argv[1]
     output_folder = sys.argv[2]
-    print(f'Will extract stats of {gzvcf_file}, output to: {output_folder}')
+    output_file_prefix = output_folder + sys.argv[3] # like chr9
+    print(f'Will extract stats of {gzvcf_file}, output to: {output_file_prefix}')
 
     os.makedirs(output_folder, exist_ok=True)
 
-    cmd_parts_base = ['vcftools', '--gzvcf', gzvcf_file, '--out', output_folder, '--max-alleles', '2', '--min-alleles', '2']
+    cmd_parts_base = ['vcftools', '--gzvcf', gzvcf_file, '--max-alleles', '2', '--min-alleles', '2']
 
     print(cmd_parts_base)
 
     # Calculate mean depth per individual
-    depth_i_cmd = cmd_parts_base + ['--depth',]
+    depth_i_cmd = cmd_parts_base + ['--depth', '--out', output_file_prefix + '.idepth']
     print('depth_i_cmd')
     subprocess.run(depth_i_cmd)
 
     # Calculate mean depth per site
-    depth_s_cmd = cmd_parts_base + ['--site-mean-depth',]
+    depth_s_cmd = cmd_parts_base + ['--site-mean-depth', '--out', output_file_prefix + '.ldepth']
     print('depth_s_cmd')
     subprocess.run(depth_s_cmd)
 
     # Calculate site quality
-    quality_s_cmd = cmd_parts_base + ['--site-quality',]
+    quality_s_cmd = cmd_parts_base + ['--site-quality', '--out', output_file_prefix + '.lqual']
     print('quality_s_cmd')
     subprocess.run(quality_s_cmd)
 
     # Calculate proportion of missing data per individual
-    missing_i_cmd = cmd_parts_base + ['--missing-indv',]
+    missing_i_cmd = cmd_parts_base + ['--missing-indv', '--out', output_file_prefix + '.imiss']
     print('missing_i_cmd')
     subprocess.run(missing_i_cmd)
 
     # Calculate proportion of missing data per site
-    missing_s_cmd = cmd_parts_base + ['--missing-site',]
+    missing_s_cmd = cmd_parts_base + ['--missing-site', '--out', output_file_prefix + '.lmiss']
     print('missing_s_cmd')
     subprocess.run(missing_s_cmd)
 

85d9a4ee4b092abff2994853739c99b93a110f93
Amir Rubin <amrubin@microsoft.com>
2021-01-11 22:57:07 +0200

    not stable

--- cluster/submit_split_vcfs.sh
+++ cluster/submit_split_vcfs.sh
@@ -1,18 +1,81 @@
-TODO
 #!/bin/bash
-# this will go over vcfs in the target folde, and will split according to the vcftools params
-# params
-# data="hgdp"
-# output_folder="todo"
-# mac_values=2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
-# maf_values=1,2,3..,48,49,50
-# specific_input="todo" chr22
 
-# first, create the output folder
-mkdir output_folder
-if specific_input:
-    sbatch ../split_vcfs.sh specific_input output_folder mac_values maf_values
-else:
-    #todo go over vcfs in data
-    for vcffile in vcfs:
-        sbatch ../split_vcfs.sh vcffile output_folder mac_values maf_values
+# will submit split_vcfs of given vcfs, using given mac and mafs values
+# --error="/vol/sci/bio/data/gil.greenbaum/amir.rubin/logs/cluster/split_vcfs/stderr/split_vcfs_chr_N_mac_N.stderr"
+# --output="/vol/sci/bio/data/gil.greenbaum/amir.rubin/logs/cluster/split_vcfs/stdout/split_vcfs_chr_N_mac_N.stdout"
+# --job-name=split_vcfs_chr_N_mac_N
+# --account=amirr
+# NOT READY - submit_split_vcfs.sh "/vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/" "hgdp_wgs.20190516.full.chr1.vcf.gz,hgdp_wgs.20190516.full.chr2.vcf.gz,hgdp_wgs.20190516.full.chr3.vcf.gz,hgdp_wgs.20190516.full.chr4.vcf.gz,hgdp_wgs.20190516.full.chr5.vcf.gz,hgdp_wgs.20190516.full.chr6.vcf.gz,hgdp_wgs.20190516.full.chr7.vcf.gz,hgdp_wgs.20190516.full.chr8.vcf.gz,hgdp_wgs.20190516.full.chr9.vcf.gz,hgdp_wgs.20190516.full.chr10.vcf.gz,hgdp_wgs.20190516.full.chr11.vcf.gz,hgdp_wgs.20190516.full.chr12.vcf.gz,hgdp_wgs.20190516.full.chr13.vcf.gz,hgdp_wgs.20190516.full.chr14.vcf.gz,hgdp_wgs.20190516.full.chr15.vcf.gz,hgdp_wgs.20190516.full.chr16.vcf.gz,hgdp_wgs.20190516.full.chr17.vcf.gz,hgdp_wgs.20190516.full.chr18.vcf.gz,hgdp_wgs.20190516.full.chr19.vcf.gz,hgdp_wgs.20190516.full.chr20.vcf.gz,hgdp_wgs.20190516.full.chr21.vcf.gz,hgdp_wgs.20190516.full.chr22.vcf.gz" "chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22"
+# submit_split_vcfs.sh "/vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/" "hgdp_wgs.20190516.full.chr22.vcf.gz hgdp_wgs.20190516.full.chr21.vcf.gz" "chr22 chr21" 2 3 1 1 49 1
+vcfs_folder=$1
+vcfs_files=$2
+vcf_files_short_names=$3
+
+# by mac
+mac_min_range=$4
+mac_max_range=$5
+mac_delta=$6
+
+# by maf
+maf_min_range=$7
+maf_max_range=$8
+maf_delta=$9
+
+# submission details
+#job_mem
+#job_time
+#job_comment=
+vcftools_params="--max-alleles 2 --min-alleles 2 --remove-indels --max-missing 0.9 --012"
+job_stderr_folder="/vol/sci/bio/data/gil.greenbaum/amir.rubin/logs/cluster/split_vcfs/stderr/"
+job_stdout_folder="/vol/sci/bio/data/gil.greenbaum/amir.rubin/logs/cluster/split_vcfs/stdout/"
+
+# print the inputs
+echo "vcfs_folder: $vcfs_folder"
+echo "vcfs_files: $vcfs_files"
+echo "vcf_files_short_names: $vcf_files_short_names"
+
+echo "mac_min_range: $mac_min_range"
+echo "mac_max_range: $mac_max_range"
+echo "mac_delta: $mac_delta"
+
+echo "maf_min_range: $maf_min_range"
+echo "maf_max_range: $maf_max_range"
+echo "maf_delta: $maf_delta"
+
+
+echo "mkdirs - $job_stderr_folder"
+#TODO
+echo "mkdirs - $job_stdout_folder"
+#TODO
+
+vcfs_files_arr=($vcfs_files)
+vcf_files_short_names_arr=($vcf_files_short_names)
+if [ ${#vcfs_files_arr[@]} != ${#vcf_files_short_names_arr[@]} ]
+then
+    echo "vcfs_files and vcf_files_short_names length should be the same"
+    exit 
+fi
+
+echo "Go over the vcf files, submit for each mac and maf a job"
+for i in "${!vcfs_files_arr[@]}"; do
+    vcf_file=${vcfs_files_arr[i]}
+    vcf_short_name=${vcf_files_short_names_arr[i]}
+    full_vcf_file_path=${vcfs_folder}${vcf_file}
+
+    # TODO - do we need to create these?
+    output_folder="${vcfs_folder}classes/${vcf_short_name}/"
+    echo "process ${vcf_file}, short name: ${vcf_short_name}"
+
+    echo "go over mac"
+    for mac in $(seq $mac_min_range $mac_delta $mac_max_range); do
+        job_stderr_file="${job_stderr_folder}${vcf_short_name}_mac${mac}.stderr"
+        job_stdout_file="${job_stdout_folder}${vcf_short_name}_mac${mac}.stdout"
+        job_name="${vcf_short_name}_mac${mac}"
+        cluster_setting='sbatch --error="'${job_stderr_file}'" --output="'${job_stdout_file}'" --job-name="'${job_name}'"'
+        cmd_to_run=${cluster_setting}' ../split_vcfs.sh "'${full_vcf_file_path}'" "'${vcftools_params}'" "'${output_folder}'" '$mac' '$mac' 1 - - -'
+        echo ${cmd_to_run}
+        eval ${cmd_to_run}
+    done
+    echo "go over maf"
+    #TODO
+done
--- split_vcfs.sh
+++ split_vcfs.sh
@@ -1,32 +1,77 @@
-TODO validate and add descrption
 #!/bin/bash
 #SBATCH --mem=4G
 #SBATCH --time=24:0:0
+#SBATCH --comment=="Split vcf file using mac and/or maf values"
 
-# To run this use:
-# sbatch split_vcfs.sh "/vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/hgdp_wgs.20190516.full.chr22.vcf.gz" "--max-alleles 2 --min-alleles 2 --remove-indels --max-missing 0.9 --recode" "/vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/classes/chr22/" "2 3 4 .. 18" "1 2 3 .. 49" 1
+
+# Will split the given vcf.gz file according to the mac (minor allele count) and maf (minor allele freq)
+
+# Example:
+# sbatch split_vcfs.sh "/vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/hgdp_wgs.20190516.full.chr22.vcf.gz" "--max-alleles 2 --min-alleles 2 --remove-indels --max-missing 0.9 --012" "/vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/classes/chr22/" "2 3 4 .. 18" "1 2 3 .. 49" 1
+# sbatch split_vcfs.sh "/vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/hgdp_wgs.20190516.full.chr22.vcf.gz" "--max-alleles 2 --min-alleles 2 --remove-indels --max-missing 0.9 --012" "/vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/classes/chr22/" 2 2 1 - - -
+
+module load bio
 
 # params
 vcffile=$1
-# example vcftools_params: "--max-alleles 2 --min-alleles 2 --remove-indels --max-missing 0.9 --recode"
+# example vcftools_params: "--max-alleles 2 --min-alleles 2 --remove-indels --max-missing 0.9 --012"
 vcftools_params=$2
 output_folder=$3
-mac_values=$4
-maf_values=$5
-maf_delta=$6
+# by mac
+mac_min_range=$4
+mac_max_range=$5
+mac_delta=$6
+# by maf
+maf_min_range=$7
+maf_max_range=$8
+maf_delta=$9
 
-for mac in $mac_values
+
+echo "vcffile: $vcffile"
+echo "vcftools_params: $vcftools_params"
+echo "output_folder: $output_folder"
+
+echo "mac_min_range: $mac_min_range"
+echo "mac_max_range: $mac_max_range"
+echo "mac_delta: $mac_delta"
+
+echo "maf_min_range: $maf_min_range"
+echo "maf_max_range: $maf_max_range"
+echo "maf_delta: $maf_delta"
+
+for mac in $(seq $mac_min_range $mac_delta $mac_max_range)
 do
-    # example cmd: vcftools --gzvcf  --mac 2 --max-mac 2 --max-alleles 2 --min-alleles 2 --remove-indels --max-missing 0.9 --recode --out /vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/classes/chr22/mac_2
-    vcfcmd="vcftools "$vcftools_params" --mac "$mac" --max-mac "$mac" --gzvcf "$vcffile" --out "$output_folder"mac_"$mac
-    echo "$vcfcmd"
-    # eval "$vcfcmd"
+    # TODO - we only perform this if the file does not exist
+    output_file="${output_folder}mac_$mac.log"
+    if [ -f "$output_file" ]; then
+        echo "$output_file exists."
+    else 
+        echo "Will execute vcftools - $output_file does not exist."
+        # example cmd: vcftools --gzvcf  --mac 2 --max-mac 2 --max-alleles 2 --min-alleles 2 --remove-indels --max-missing 0.9 --recode --out /vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/classes/chr22/mac_2
+        # we create a folder for temp output
+        mkdir $output_folder"temp_mac_"$mac
+        # as mac are integers, we take the range to be [mac, mac + delta ** - 1 **]
+        # otherwise, for delta=1, we would have classes of mac in [2,3] (the --mac and --max-mac are inclusive to the value)
+
+        vcfcmd="vcftools $vcftools_params --mac $mac --max-mac $(($mac+$mac_delta-1)) --gzvcf $vcffile --out ${output_folder}mac_$mac --temp ${output_folder}temp_mac_$mac"
+        echo "$vcfcmd"
+        #eval "$vcfcmd"
+    fi
 done
 
-for mac in $mac_values
+for maf in $maf_values
 do
-    # example cmd: vcftools --gzvcf  --mac 2 --max-mac 2 --max-alleles 2 --min-alleles 2 --remove-indels --max-missing 0.9 --recode --out /vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/classes/chr22/mac_2
-    vcfcmd="vcftools "$vcftools_params" --maf "$maf" --max-maf "$(($maf+$maf_delta))" --gzvcf "$vcffile" --out "$output_folder"maf_"$maf
-    echo "$vcfcmd"
-    # eval "$vcfcmd"
+    # TODO - we only perform this if the file does not exist
+    output_file="${output_folder}maf_$maf.log"
+    if [ -f "$output_file" ]; then
+        echo "$output_file exists."
+    else 
+        echo "Will execute vcftools - $output_file does not exist."
+        # example cmd: vcftools --gzvcf  --maf 2 --max-maf 3 --max-alleles 2 --min-alleles 2 --remove-indels --max-missing 0.9 --recode --out /vol/sci/bio/data/gil.greenbaum/amir.rubin/vcf/hgdp/classes/chr22/maf_2
+        # we create a folder for temp output
+        mkdir $output_folder"temp_maf_"$maf
+        vcfcmd="vcftools $vcftools_params --maf $maf --max-maf $(($maf+$maf_delta)) --gzvcf $vcffile --out ${output_folder}maf_$maf --temp ${output_folder}temp_maf_$maf"
+        echo "$vcfcmd"
+        #eval "$vcfcmd"
+    fi
 done

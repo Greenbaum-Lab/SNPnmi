5716f03fd44c7b46b13f1061d3abf1a8231635f0
shahar.mazie <shahar.mazie@mail.huji.ac.il>
2021-07-19 12:07:49 +0300

    Change step 3.3 to run with options

--- steps/s3_split_to_windows/submit_merge_all_chrs_to_class_windows.py
+++ steps/s3_split_to_windows/submit_merge_all_chrs_to_class_windows.py
@@ -1,3 +1,5 @@
+from utils.loader import Loader
+
 DEBUG = False
 # Per class and a number N, will submit jobs which will each merge N windows.
 # For example, for class mac 18, and max_num_of_windows_per_job=10, assuming in total the class has 101 (0-100) windows, we will submit 11 jobs:
@@ -8,7 +10,7 @@ import os
 from os.path import dirname, abspath
 root_path = dirname(dirname(dirname(abspath(__file__))))
 sys.path.append(root_path)
-from utils.common import get_paths_helper, AlleleClass
+from utils.common import get_paths_helper, AlleleClass, args_parser, are_running_submitions
 from utils.config import *
 from utils.cluster.cluster_helper import submit_to_cluster
 from utils.checkpoint_helper import *
@@ -28,30 +30,35 @@ def get_num_windows_per_class(dataset_name, mac_maf, class_value):
         print(f'Class {allele_class.class_name} has {total_num_of_windows} windows')
         return total_num_of_windows
 
-def submit_merge_all_chrs_to_class_windows(dataset_name, mac_min_range, mac_max_range, maf_min_range, maf_max_range, max_num_of_windows_per_job):
-    paths_helper = get_paths_helper(dataset_name)
+def submit_merge_all_chrs_to_class_windows(options):
+    dataset_name = options.dataset_name
+    mac_min_range, mac_max_range, maf_min_range, maf_max_range, max_num_of_windows_per_job = options.args
 
     for mac_maf in ['mac', 'maf']:
         is_mac = mac_maf == 'mac'
         min_range = mac_min_range if is_mac else maf_min_range
         max_range = mac_max_range if is_mac else maf_max_range
-        if min_range>0:
+        if min_range > 0:
             # Go over mac/maf values
             print(f'Go over {mac_maf} values: [{min_range},{max_range}]')
             for class_int_val in range(min_range, max_range+1):
                 total_num_windows = get_num_windows_per_class(dataset_name, mac_maf, class_int_val)
                 for min_windows_index in range(0, total_num_windows, max_num_of_windows_per_job):
-                    max_windows_index = min(total_num_windows ,min_windows_index + max_num_of_windows_per_job) - 1
+                    max_windows_index = min(total_num_windows, min_windows_index + max_num_of_windows_per_job) - 1
                     print(f'submit for {mac_maf} {class_int_val} windows [{min_windows_index}, {max_windows_index}]')
                     job_long_name = generate_job_long_name(mac_maf, class_int_val, min_windows_index, max_windows_index)
-                    job_name=f'3s{mac_maf}{class_int_val}{min_windows_index}'
-                    python_script_params = f'{dataset_name} {mac_maf} {class_int_val} {min_windows_index} {max_windows_index}'
-                    submit_to_cluster(dataset_name, job_type, job_long_name, job_name, path_to_python_script_to_run, python_script_params, with_checkpoint=False, num_hours_to_run=24, debug=DEBUG)
+                    job_name = f'3s{mac_maf}{class_int_val}{min_windows_index}'
+                    python_script_params = f'-d {dataset_name} --args {mac_maf},{class_int_val},{min_windows_index},{max_windows_index}'
+                    submit_to_cluster(dataset_name, job_type, job_long_name, job_name, path_to_python_script_to_run,
+                                      python_script_params, with_checkpoint=False, num_hours_to_run=24, debug=DEBUG)
 
-def main(args):
+        with Loader("Wait for all merging jobs to be done "):
+            while are_running_submitions(string_to_find="3sma"):
+                time.sleep(5)
+
+def main(options):
     s = time.time()
-    dataset_name = args[0]
-    submit_merge_all_chrs_to_class_windows(*args)
+    submit_merge_all_chrs_to_class_windows(options)
     print(f'{(time.time()-s)/60} minutes total run time')
     return True
 
@@ -61,4 +68,5 @@ def _test_me():
 if DEBUG:
     _test_me()
 elif __name__ == '__main__':
-    main(sys.argv[1:])
+    options = args_parser()
+    main(options)

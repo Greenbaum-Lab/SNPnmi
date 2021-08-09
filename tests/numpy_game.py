517560afbba4790cae25f7b25f097600fea277e6
shahar.mazie <shahar.mazie@mail.huji.ac.il>
2021-08-09 17:59:52 +0300

    Merge remote-tracking branch 'origin/shahar' into shahar

--- tests/numpy_game.py
+++ tests/numpy_game.py
@@ -2,8 +2,13 @@ import json
 
 import numpy as np
 
+from utils.common import get_paths_helper
+
 with open("mat.json", 'w') as f:
     json.dump({}, f)
-with open("mat1.json", "w+") as f:
+class_str = 'mac_8'
+dataset = 'hgdp_test'
+paths_helper = get_paths_helper(dataset)
+with open(paths_helper.hash_windows_list_template.format(class_name=class_str), "w") as f:
     json.dump({}, f)
 print("done")

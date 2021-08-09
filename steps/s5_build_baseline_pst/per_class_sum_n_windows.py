a888973ab8fe72b440070baa659413f0f98f92a0
shahar.mazie <shahar.mazie@mail.huji.ac.il>
2021-08-09 17:44:49 +0300

    Merge remote-tracking branch 'origin/shahar' into shahar

--- steps/s5_build_baseline_pst/per_class_sum_n_windows.py
+++ steps/s5_build_baseline_pst/per_class_sum_n_windows.py
@@ -36,16 +36,16 @@ def handle_hash_file(class_name, paths_helper, windows_id_list):
     hash_file = paths_helper.hash_windows_list_template.format(class_name=class_name)
     with FileLock(hash_file):
         if not os.path.exists(hash_file):
-            with open(hash_file, "w") as f:
-                json.dump({"0": []}, f)
+            with open(hash_file, "w+") as f:
+                json.dump({}, f)
                 print("I opened the hash file")
-        with open(hash_file, "r") as jsonFile:
+        with open(hash_file, "r+") as jsonFile:
             data = json.load(jsonFile)
             hash_codes = [int(i) for i in data.keys()]
             new_hash = 0 if len(hash_codes) == 0 else 1 + max(hash_codes)
             if windows_id_list not in data.values():
                 data[new_hash] = windows_id_list
-                with open(hash_file, "w") as f:
+                with open(hash_file, "w+") as f:
                     json.dump(data, f)
     return new_hash
 

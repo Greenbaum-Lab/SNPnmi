33012af17f76e2ff1aaccfe950c96a10222bf22d
shahar.mazie <shahar.mazie@mail.huji.ac.il>
2021-08-19 15:20:21 +0300

    Try a bug fix

--- utils/checkpoint_helper.py
+++ utils/checkpoint_helper.py
@@ -3,7 +3,7 @@ import os
 from os.path import dirname, abspath
 root_path = dirname(dirname(abspath(__file__)))
 sys.path.append(root_path)
-from utils.common import get_paths_helper, hash_args
+from utils.common import get_paths_helper, hash_args, str_for_timer
 from utils.config import validate_dataset_name
 from datetime import datetime
 
@@ -39,9 +39,7 @@ def checkpoint_file_check(dataset_name, checkpoint_name, options):
 # 1. the first argument in args is dataset_name
 # 2. func accepts args and returns bool
 def execute_with_checkpoint(func, checkpoint_name, options):
-    print('Dataset name', options.dataset_name)
-    print('Number of arguments:', len(options.args))
-    print('Argument List:', str(options.args))
+    print(f'Argument List: {options.dataset_name}, {str_for_timer(options)}')
     assert validate_dataset_name(options.dataset_name), f'{options.dataset_name} is not a known dataset name'
     is_checkpoint_exists, checkpoint_file = checkpoint_file_check(options.dataset_name, checkpoint_name, options)
     if is_checkpoint_exists:

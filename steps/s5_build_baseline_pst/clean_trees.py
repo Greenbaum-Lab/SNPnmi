f73116d5400c01bf317d0dc8a6924dc8017b612e
shahar.mazie <shahar.mazie@mail.huji.ac.il>
2021-10-25 19:15:31 +0300

    clean trees suppos to be ready. Should be tested well. We dont want to delete good files and dirs!

--- steps/s5_build_baseline_pst/clean_trees.py
+++ steps/s5_build_baseline_pst/clean_trees.py
@@ -29,7 +29,7 @@ def track_invalid_hashes_per_class(options, paths_helper, class_name):
         elif not os.path.isdir(ns_dir + f'_{k}') or not os.listdir(ns_dir + f'_{k}'):
             invalid_hashes.append(k)
     if invalid_hashes:
-        print(f"Deleting for class {class_name} the next hashes: {invalid_hashes} form {hash_dict.keys()}")
+        print(f"Deleting for class {class_name} the next hashes: {invalid_hashes} form {list(hash_dict.keys())}")
     return invalid_hashes
 
 

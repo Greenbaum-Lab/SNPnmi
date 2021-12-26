babe6115a844cda2640e2f950f4d45d2eaa36aa9
g n <golan.nahaliel@mail.huji.ac.il>
2021-12-26 17:44:21 +0200

    bug fix of file too big to open

--- utils/common.py
+++ utils/common.py
@@ -55,7 +55,7 @@ class AlleleClass:
 def is_class_valid(options, mac_maf, class_int_value):
     if mac_maf == 'maf' and class_int_value > 49:
         return False
-    if options.dataset_name == 'arabidopsis' and mac_maf == 'maf' and class_int_value % 2 == 1:
+    if options.dataset_name == 'arabidopsis' and mac_maf == 'mac' and class_int_value % 2 == 1:
         return False
     return True
 

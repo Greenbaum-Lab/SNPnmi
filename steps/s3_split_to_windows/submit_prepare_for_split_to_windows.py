9ac22bd6ae78fdf8d81c20a40472e0bbf47be67b
g n <golan.nahaliel@mail.huji.ac.il>
2021-12-26 17:38:13 +0200

    bug fix of file too big to open

--- steps/s3_split_to_windows/submit_prepare_for_split_to_windows.py
+++ steps/s3_split_to_windows/submit_prepare_for_split_to_windows.py
@@ -65,6 +65,8 @@ def submit_prepare_for_split_to_windows(options):
             # Go over mac/maf values
             print(f'go over {mac_maf} values: [{min_range},{max_range}]')
             for class_int_val in range(min_range, max_range + 1):
+                if paths_helper.dataset_name == 'arabidopsis' and is_mac and class_int_val % 2 == 1:
+                    continue
                 classes.append(f'{mac_maf}_{class_int_val if mac_maf == "mac" else class_int_val / 100}')
                 print(f'submit for {classes[-1]}')
                 job_long_name = generate_job_long_name(mac_maf, class_int_val)

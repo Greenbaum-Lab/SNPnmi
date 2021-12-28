c0fd95e0d0ea1c441510b433e40a8c1a62717db1
g n <golan.nahaliel@mail.huji.ac.il>
2021-12-28 14:13:27 +0200

    Add option to run few netstructs on a single node

--- utils/common.py
+++ utils/common.py
@@ -95,7 +95,7 @@ def handle_hash_file(class_name, paths_helper, windows_id_list):
     data = load_dict_from_json(hash_file)
     with FileLock(hash_file):
         hash_codes = [int(i) for i in data.keys()]
-        new_hash = 0 if len(hash_codes) == 0 else 1 + max(hash_codes)
+        new_hash = 0 if len(hash_codes) == 0 else min([i+1 for i in hash_codes if (i+1) not in hash_codes])
         if windows_id_list not in data.values():
             data[str(new_hash)] = windows_id_list
             with open(hash_file, "w") as f:

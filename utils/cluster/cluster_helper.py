b025503c7fea0267be580b92d989a13fb5be82c1
shahar.mazie <shahar.mazie@mail.huji.ac.il>
2022-01-04 14:40:20 +0200

    add ulimit 2048 for step 2.1

--- utils/cluster/cluster_helper.py
+++ utils/cluster/cluster_helper.py
@@ -27,8 +27,8 @@ def submit_to_cluster(options, job_type, job_name, script_path, script_args, job
 
     cluster_setting = f'sbatch --time={num_hours_to_run}:00:00 --mem={memory}G --error="{job_stderr_file}" --output="{job_stdout_file}" --job-name="{job_name}"'
     assert len(script_args.split()) <= MAX_PARAMS_SUPPORTED
-    warpper = choose_wrapper(options, paths_helper)
-    cmd_to_run = f'{cluster_setting} {paths_helper.wrapper_max_30_params} python3 {script_path} {script_args}'
+    wrapper = choose_wrapper(options, paths_helper)
+    cmd_to_run = f'{cluster_setting} {wrapper} python3 {script_path} {script_args}'
     options.submit_to_cluster_args = [paths_helper.submit_helper, cmd_to_run]
     execute_with_checkpoint(submit_wrapper, job_type, options)
 

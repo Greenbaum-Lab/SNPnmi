c010f46b93568cb223762783a0f73126d61a6637
Shahar <shahar.mazie@mail.huji.ac.il>
2022-01-10 20:26:40 +0200

    refactor plot script

--- notebooks/plots_notebooks/number_of_snp_per_class.py
+++ notebooks/plots_notebooks/number_of_snp_per_class.py
@@ -10,24 +10,28 @@ from utils.common import is_cluster
 import matplotlib.pyplot as plt
 import json
 import numpy as np
-if is_cluster():
-    summary_dir = r"/vol/sci/bio/data/gil.greenbaum/shahar.mazie/vcf/hgdp/classes/summary/"
-    windows_dir = r"/vol/sci/bio/data/gil.greenbaum/shahar.mazie/vcf/hgdp/classes/windows/"
-else:
-    summary_dir = r"/home/lab2/shahar/cluster_dirs/vcf/hgdp/classes/summary/"
-    windows_dir = r"/home/lab2/shahar/cluster_dirs/vcf/hgdp/classes/windows/"
+import sys
+from os.path import dirname, abspath
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
-window_to_snps_path =  windows_dir + 'number_of_windows_per_class.txt'
-window_size_path = windows_dir + 'window_size.txt'
-mac_min_range = 2
-mac_max_range = 70
-maf_min_range = 1
-maf_max_range = 49
-mac_class_names = np.arange(mac_min_range, mac_max_range+1)
-maf_class_names = np.arange(maf_min_range, maf_max_range+1) / 100
+root_path = dirname(dirname(dirname(abspath(__file__))))
+sys.path.append(root_path)
+
+from utils.common import args_parser, get_paths_helper, is_class_valid
+
+options = args_parser()
+paths_helper = get_paths_helper(options.dataset_name)
 
 
-# In[23]:
+mac_min_range, mac_max_range = options.mac
+maf_min_range, maf_max_range = options.maf
+window_to_snps_path = paths_helper.windows_dir + 'number_of_windows_per_class.txt'
+window_size_path = paths_helper.windows_dir + 'window_size.txt'
+mac_class_names = np.arange(mac_min_range, mac_max_range + 1) if options.dataset_name != 'arabidopsis' else np.arange(
+    mac_min_range, mac_max_range + 1, 2)
+maf_class_names = np.arange(maf_min_range, maf_max_range + 1) / 100
 
 
 with open(window_to_snps_path) as f:
@@ -40,6 +44,8 @@ for mac_maf in ['mac', 'maf']:
     min_range = mac_min_range if is_mac else maf_min_range
     max_range = mac_max_range if is_mac else maf_max_range
     for val in range(min_range, max_range+1):
+        if not is_class_valid(options, mac_maf, val):
+            continue
         # in maf we take 0.x
         if not is_mac:
             val = f'{val * 1.0/100}'
@@ -50,7 +56,7 @@ for mac_maf in ['mac', 'maf']:
     plt.yscale('log')
     plt.xlabel(f"{mac_maf}")
     plt.title('Num of SNPs per class')
-    plt.savefig(f'{summary_dir}num_of_snps/{mac_maf}.svg')
+    plt.savefig(f'{paths_helper.summary_dir}num_of_snps/{mac_maf}.svg')
     plt.clf()
     num_of_snps = []
 

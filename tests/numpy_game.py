975b11aa0690bd5dc7dddbe3c9e6e767240d80d5
g n <golan.nahaliel@mail.huji.ac.il>
2022-03-14 16:03:33 +0200

    numpy games

--- tests/numpy_game.py
+++ tests/numpy_game.py
@@ -17,6 +17,13 @@ def numpy_compress(matrix):
     bin_mat[:, :, 1] = matrix % 2 == 0
     return bin_mat
 
+def numpy_decompress(bin_mat):
+    decomp_mat = np.empty(shape=(bin_mat.shape[0], bin_mat.shape[1]), dtype=np.int8)
+    decomp_mat[:, :] = bin_mat[:, :, 0] & ~ bin_mat[:, :, 1]
+    decomp_mat[:, :] -= ~bin_mat[:, :, 0] & ~ bin_mat[:, :, 1]
+    decomp_mat[:, :] += 2 * (bin_mat[:, :, 0] & bin_mat[:, :, 1])
+    return decomp_mat
+
 
 path = '/sci/labs/gilig/shahar.mazie/icore-data/vcf/sim_2_v0/classes/windows/maf_0.39/chr1/'
 
@@ -27,8 +34,14 @@ mat = np.load(path + 'window_93.012.npy')
 #                 [0, -1, 0, 1, 0, -1, 1, 2, 1, -1, 0, 1, 0, -1, 0, 1],
 #                 [0, -1, 0, 1, 0, -1, 0, -1, 0, -1, 0, 1, 1, -1, 0, 1],
 #                 [0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, 2, 0, 1]], dtype=np.int8)
-bin_mat = numpy_compress(mat)
+compressed = numpy_compress(mat)
+bin_mat = np.packbits(compressed)
 np.save(path + 'bin_exp.npy', bin_mat)
 # np.save('/media/lab-heavy/Data/shahar/SNPnmi/tests/exp_bin.npy', bin_mat)
 # np.save('/media/lab-heavy/Data/shahar/SNPnmi/tests/exp_int_np.npy', mat)
 
+# bin_mat_load = np.load('/media/lab-heavy/Data/shahar/SNPnmi/tests/exp_bin.npy')
+bin_mat_load = np.load(path + 'bin_exp.npy')
+unpacked = np.unpackbits(bin_mat).astype(np.bool).reshape((mat.shape[0], mat.shape[1], 2))
+renew = numpy_decompress(unpacked)
+print(np.all(renew == mat))

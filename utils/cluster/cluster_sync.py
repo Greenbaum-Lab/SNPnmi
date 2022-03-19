cde677a9511d5e19731d39ff88c61d99c5f548ac
Shahar <shahar.mazie@mail.huji.ac.il>
2022-03-19 17:28:39 +0200

    test sync function

--- utils/cluster/cluster_sync.py
+++ utils/cluster/cluster_sync.py
@@ -40,8 +40,7 @@ def del_dir(source_list):
         job_stdout_file = f'/sci/labs/gilig/shahar.mazie/icore-data/tmp/cluster_out_files/rm_{source}'
         delete_line = f'rm -rf /sci/labs/gilig/shahar.mazie/icore-data/vcf/{source}'
         job_name = 'rm' + source[:-3]
-        cluster_setting = f'sbatch --time={num_hours_to_run}:00:00 --mem={memory}G --error="{job_stderr_file}' \
-                          f'" --output="{job_stdout_file}" --job-name="{job_name}"'
+        cluster_setting = f'sbatch --time={num_hours_to_run}:00:00 --mem={memory}G --error="{job_stderr_file}'
         subprocess.run([submit_helper_path, f'{cluster_setting} {warp_30_params_path} {delete_line}'])
     with Loader(f"uploading {dirname(dirname(source_list[0]))}", string_to_find="rm"):
         while how_many_jobs_run(string_to_find="rm"):

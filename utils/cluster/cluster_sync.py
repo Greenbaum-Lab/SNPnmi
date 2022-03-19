b4152d0ad7e93b3361c1743efde999ec33044398
Shahar <shahar.mazie@mail.huji.ac.il>
2022-03-19 13:44:29 +0200

    test sync function

--- utils/cluster/cluster_sync.py
+++ utils/cluster/cluster_sync.py
@@ -10,22 +10,43 @@ sys.path.append(root_path)
 from utils.loader import Loader
 from utils.common import how_many_jobs_run
 
-def sync_dir(source):
+def sync_dir(source_list):
     num_hours_to_run = 2
     memory = 8
-    job_stderr_file = f'/sci/labs/gilig/shahar.mazie/icore-data/tmp/cluster_err_files/{source}'
-    job_stdout_file = f'/sci/labs/gilig/shahar.mazie/icore-data/tmp/cluster_out_files/{source}'
-    job_name = 'dr' + source[:-3]
-    cluster_setting = f'sbatch --time={num_hours_to_run}:00:00 --mem={memory}G --error="{job_stderr_file}' \
-                      f'" --output="{job_stdout_file}" --job-name="{job_name}"'
-    cmd_line = f'rclone sync /sci/labs/gilig/shahar.mazie/icore-data/vcf/{source} remote:gili_lab/vcf/{source}'
-    warp_30_params_path = '/sci/labs/gilig/shahar.mazie/icore-data/code/snpnmi/utils/cluster/wrapper_max_30_params.sh'
-    submit_helper_path = '/sci/labs/gilig/shahar.mazie/icore-data/code/snpnmi/utils/cluster/submit_helper.sh'
-    print('Start!')
-    subprocess.run([submit_helper_path, f'{cluster_setting} {warp_30_params_path} {cmd_line}'])
-    with Loader(f"uploading {source}", string_to_find="dr"):
+    for source in source_list:
+        job_stderr_file = f'/sci/labs/gilig/shahar.mazie/icore-data/tmp/cluster_err_files/{source}'
+        job_stdout_file = f'/sci/labs/gilig/shahar.mazie/icore-data/tmp/cluster_out_files/{source}'
+        job_name = 'dr' + source[:-3]
+        cluster_setting = f'sbatch --time={num_hours_to_run}:00:00 --mem={memory}G --error="{job_stderr_file}' \
+                          f'" --output="{job_stdout_file}" --job-name="{job_name}"'
+        cmd_line = f'rclone sync /sci/labs/gilig/shahar.mazie/icore-data/vcf/{source} remote:gili_lab/vcf/{source}'
+        warp_30_params_path = '/sci/labs/gilig/shahar.mazie/icore-data/code/snpnmi/utils/cluster/wrapper_max_30_params.sh'
+        submit_helper_path = '/sci/labs/gilig/shahar.mazie/icore-data/code/snpnmi/utils/cluster/submit_helper.sh'
+        subprocess.run([submit_helper_path, f'{cluster_setting} {warp_30_params_path} {cmd_line}'])
+
+    with Loader(f"uploading dir {dirname(source_list[0])}", string_to_find="dr"):
         while how_many_jobs_run(string_to_find="dr"):
             time.sleep(5)
-    print('Done!')
+    print("Done sync!")
 
-sync_dir('test_dir')
+def del_dir(source_list):
+    warp_30_params_path = '/sci/labs/gilig/shahar.mazie/icore-data/code/snpnmi/utils/cluster/wrapper_max_30_params.sh'
+    submit_helper_path = '/sci/labs/gilig/shahar.mazie/icore-data/code/snpnmi/utils/cluster/submit_helper.sh'
+    num_hours_to_run = 2
+    memory = 8
+    for source in source_list:
+        job_stderr_file = f'/sci/labs/gilig/shahar.mazie/icore-data/tmp/cluster_err_files/rm_{source}'
+        job_stdout_file = f'/sci/labs/gilig/shahar.mazie/icore-data/tmp/cluster_out_files/rm_{source}'
+        delete_line = f'rm -rf /sci/labs/gilig/shahar.mazie/icore-data/vcf/{source}'
+        job_name = 'rm' + source[:-3]
+        cluster_setting = f'sbatch --time={num_hours_to_run}:00:00 --mem={memory}G --error="{job_stderr_file}' \
+                          f'" --output="{job_stdout_file}" --job-name="{job_name}"'
+        subprocess.run([submit_helper_path, f'{cluster_setting} {warp_30_params_path} {delete_line}'])
+        with Loader(f"uploading {source}", string_to_find="rm"):
+            while how_many_jobs_run(string_to_find="rm"):
+                time.sleep(5)
+source_list = ['/hgdp/classes/windows/maf_0.23/chr'] * 22
+for i in range(1, 23):
+    source_list[i-1] += str(i)
+sync_dir(source_list)
+

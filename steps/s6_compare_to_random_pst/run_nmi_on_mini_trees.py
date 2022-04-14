50199d20ab43c9b3faa64a687382f98e08e8dfe0
g n <golan.nahaliel@mail.huji.ac.il>
2022-04-14 14:00:46 +0300

    add data to config file from simulation runner

--- steps/s6_compare_to_random_pst/run_nmi_on_mini_trees.py
+++ steps/s6_compare_to_random_pst/run_nmi_on_mini_trees.py
@@ -35,7 +35,7 @@ def compute_nmi_scores_per_class(options, class_name, paths_helper, num_of_winds
 def run_nmi_on_classes_all_trees(options):
     paths_helper = get_paths_helper(options.dataset_name)
     window_size = get_window_size(paths_helper)
-    data_size = options.args[0]
+    data_size = int(options.args[0])
     assert data_size // window_size == data_size / window_size, "data size is not dividable in window size"
     num_of_windows = data_size // window_size
     for cls in tqdm(list(class_iter(options))):

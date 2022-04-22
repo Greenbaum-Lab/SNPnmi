4a00ff208038b19df834f6f0bf574f0877448f6f
shaharma <shahar.mazie@mail.huji.ac.il>
2022-04-22 18:33:51 +0300

    move plot nmi scores to use plot helper

--- steps/s7_join_to_summary/collect_tree_heights.py
+++ steps/s7_join_to_summary/collect_tree_heights.py
@@ -69,10 +69,9 @@ def collect_tree_heights_per_class(options, paths_helper, class_name, df):
     return df
 
 
-def collect_tree_heights(options, size):
-    print(f"collect tree heights for size {size}")
+def collect_tree_heights(options, data_size):
+    print(f"collect tree heights for size {data_size}")
     paths_helper = get_paths_helper(options.dataset_name)
-    data_size = options.args[0]
 
     os.makedirs(paths_helper.summary_dir, exist_ok=True)
     csv_path = paths_helper.summary_dir + f'/tree_heights_{data_size}_ss_{options.ns_ss}.csv'
@@ -98,10 +97,10 @@ def combine_attributes_per_class(class_name, input_df, sum_df):
     return sum_df
 
 
-def combine_heights_to_sum_matrix(options, full_mat_df, size):
-    print(f"combine heights to sum matrix for size {size}")
+def combine_heights_to_sum_matrix(options, full_mat_df, data_size):
+    print(f"combine heights to sum matrix for size {data_size}")
     paths_helper = get_paths_helper(options.dataset_name)
-    csv_output_path = paths_helper.summary_dir + f'/tree_heights_per_class_{size}.csv'
+    csv_output_path = paths_helper.summary_dir + f'/tree_heights_per_class_{data_size}.csv'
     sum_mat_df = pd.DataFrame(columns=['Class', 'avg_max_height', 'std_max_height', 'avg_avg_height', 'std_avg_height',
                                        'avg_avg_leaves', 'std_avg_leaves'])
     for cls in tqdm(list(class_iter(options)), desc='collect tree height Stage 2'):
@@ -113,9 +112,9 @@ def combine_heights_to_sum_matrix(options, full_mat_df, size):
 
 def main(options):
     with Timer(f"Collect tree heights to csv"):
-        for size in ['1000', '5000']:
-            df = collect_tree_heights(options, size)
-            combine_heights_to_sum_matrix(options, df, size)
+        for data_size in ['1000', '5000']:
+            df = collect_tree_heights(options, data_size)
+            combine_heights_to_sum_matrix(options, df, data_size)
 
 
 if __name__ == "__main__":

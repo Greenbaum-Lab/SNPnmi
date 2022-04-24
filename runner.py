55de763b9cf760bf9c57390d91b60d7481ae6235
Shahar <shahar.mazie@mail.huji.ac.il>
2022-04-25 00:10:22 +0300

    remove saving of extra numpy arrays

--- runner.py
+++ runner.py
@@ -66,9 +66,9 @@ def run_all_pipeline(options):
             _options.args = [_orig_args[1]]  # num_of_winds_per_job
 
         elif _step in ['5.4.1', '6.2.1']:
-            _options.args = [str(_options.data_size[0]), '5']
+            _options.args = [str(_options.data_size[0]), '100']
         elif _step in ['5.4.2', '6.2.2']:
-            _options.args = [str(_options.data_size[1]), '5']
+            _options.args = [str(_options.data_size[1]), '100']
         else:
             _options.args = _orig_args
         return _options

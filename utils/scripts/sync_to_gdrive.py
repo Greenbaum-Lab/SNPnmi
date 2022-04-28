31725f5d437e386d605d9b338d8292f2a826fc83
g n <golan.nahaliel@mail.huji.ac.il>
2022-04-28 11:02:06 +0300

    Add tree plots to pipeline

--- utils/scripts/sync_to_gdrive.py
+++ utils/scripts/sync_to_gdrive.py
@@ -11,13 +11,25 @@ sys.path.append(root_path)
 from utils.common import args_parser
 
 
-def sync_cluster_to_google_drive(options, source_file, dest_file):
-    subprocess.run(['rclone', 'sync', source_file, dest_file])
-    subprocess.run(['rclone', 'check', source_file, dest_file])
+def sync_cluster_to_google_drive(source_file, dest_dir, dest_file):
+    if dest_file:
+        # subprocess.run(['rclone', 'sync', source_file, dest_dir + dest_file])
+        pass
+    else:
+        pass
+        # subprocess.run(['rclone', 'sync', source_file, dest_dir])
+    output = subprocess.run(['rclone', 'check', source_file, dest_dir], check=True, capture_output=True)
+    print(output)
+    print(type(output))
+    output = str(output)
+    print(output)
+    print(type(output))
+    print(len(output))
 
 
 def main(options):
-    sync_cluster_to_google_drive(options, options.args[0], options.args[1])
+    dest_file = options.args[2] if len(options.args) == 3 else None
+    sync_cluster_to_google_drive(options.args[0], options.args[1], dest_file)
 
 
 if __name__ == "__main__":

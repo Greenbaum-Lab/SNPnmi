543d44439ccd06035b405d89b6a1939f603940e1
Shahar <shahar.mazie@mail.huji.ac.il>
2022-06-09 20:28:37 +0300

    create vcf for pairwise populations

--- sfs_analysis/split_dataset_by_subpops.py
+++ sfs_analysis/split_dataset_by_subpops.py
@@ -89,12 +89,12 @@ def create_vcf_per_2_sites(options, paths_helper, site, special_list):
         if other_site not in special_list:
             continue
         print(f"start with {site} & {other_site}")
-        other_site_vcf_file = f"{paths_helper.sfs_dir}{other_site}/{other_site}.vcf.gz"
-        combined_sites_vcf_file_tmp = f"{paths_helper.sfs_dir}{site}/{site}-{other_site}_tmp.vcf.gz"
-        combined_sites_vcf_file = f"{paths_helper.sfs_dir}{site}/{site}-{other_site}.vcf.gz"
+        other_site_vcf_file = f'{paths_helper.sfs_dir}{other_site}/{other_site}.vcf.gz'
+        combined_sites_vcf_file_tmp = f'{paths_helper.sfs_dir}{site}/{site}-{other_site}_tmp.vcf.gz'
+        combined_sites_vcf_file = f'{paths_helper.sfs_dir}{site}/{site}-{other_site}.vcf.gz'
         if os.path.exists(combined_sites_vcf_file):
             continue
-        bcftools_cmd = ['bcftools', 'merge', f"'{site_vcf_file}'", f"'{other_site_vcf_file}'", '-O', 'z', '-o', combined_sites_vcf_file_tmp]
+        bcftools_cmd = ['bcftools', 'merge', f'{site_vcf_file}', f'{other_site_vcf_file}', '-O', 'z', '-o', combined_sites_vcf_file_tmp]
         print(bcftools_cmd)
         subprocess.run([paths_helper.submit_helper, ' '.join(bcftools_cmd)])
         print("Done generate tmp vcf")

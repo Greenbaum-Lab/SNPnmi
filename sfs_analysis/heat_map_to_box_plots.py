a794d8dae83e318f327e4dfd9d7409057ed01dfb
Shahar <shahar.mazie@mail.huji.ac.il>
2022-07-16 18:49:31 +0300

    First commit - boxplots

--- sfs_analysis/heat_map_to_box_plots.py
+++ sfs_analysis/heat_map_to_box_plots.py
@@ -1,29 +1,43 @@
 import itertools
+import sys
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from utils.common import args_parser, get_paths_helper
 
-
-def csv_2_box_plot(heatmap_csv, pop2continent):
+def csv_2_box_plot(heatmap_csv, pop2continent, output_dir):
     data = pd.read_csv(heatmap_csv)
     continents = set(pop2continent.values())
-    plot_data = {c: [] for c in continents}
+    two_d_continent_vals = {(c1, c2): [] for c1, c2 in itertools.combinations_with_replacement(continents, 2)}
+    for c1, c2 in itertools.combinations_with_replacement(continents, 2):
+        c1_pops = [k for k, v in pop2continent.items() if v == c1]
+        c2_pops = [k for k, v in pop2continent.items() if v == c2]
+        for p1 in c1_pops:
+            for p2 in c2_pops:
+                val = float(data.loc[data["sites"] == p1][p2])
+                if val > 0:
+                    two_d_continent_vals[(c1, c2)].append(val)
     for c in continents:
-        pops = [k for k, v in pop2continent.items() if v == c]
-        for p1, p2 in itertools.combinations(pops, 2):
-            plot_data[c].append(data[p1, p2])
-    fig1, ax1 = plt.subplots()
-    ax1.set_title('Basic Plot')
-    ax1.boxplot(plot_data)
-    plt.show()
+        fig1, ax1 = plt.subplots()
+        c_vals = {}
+        for (c1, c2), v in two_d_continent_vals.items():
+            if c1 == c:
+                c_vals[c2] = v
+            elif c2 == c:
+                c_vals[c1] = v
+        ax1.set_title(f'{c}')
+        ax1.boxplot(c_vals.values())
+        ax1.set_xticklabels(c_vals.keys(), fontsize=6)
+        plt.savefig(f"{output_dir}/{c}.svg")
+        plt.clf()
+
 
 
 def build_pop2continent(csv_file):
-    data = pd.read_csv(csv_file)
+    data = pd.read_csv(csv_file, sep='\t')
     pop2continent = {}
-    for row in data.iterrows():
+    for _, row in data.iterrows():
         pop = row.population
         continent = row.region
         if pop in pop2continent:
@@ -33,7 +47,7 @@ def build_pop2continent(csv_file):
     return pop2continent
 
 if __name__ == '__main__':
-    arguments = args_parser()
-    paths_helper = get_paths_helper(arguments.dataset_name)
-    pop2continent = build_pop2continent(arguments.args[0])
-    csv_2_box_plot(arguments.args[1], pop2continent)
+    # arguments = args_parser()
+    # paths_helper = get_paths_helper(arguments.dataset_name)
+    pop2continent = build_pop2continent(sys.argv[1])
+    csv_2_box_plot(sys.argv[2], pop2continent, sys.argv[3])

6de0c3e3f022ed30a92bc2b68abb6d540d7262db
Shahar <shahar.mazie@mail.huji.ac.il>
2022-08-25 21:53:29 +0300

    submit simulations jobs

--- simulations/sfs_simulations.py
+++ simulations/sfs_simulations.py
@@ -147,7 +147,7 @@ def submit_all_migration_rates(options, paths_helper):
 
 if __name__ == '__main__':
     options = args_parser()
-    options.dataset = 'simulations'
+    options.dataset_name = 'simulations'
     plots_base_dir = '/sci/labs/gilig/shahar.mazie/icore-data/sfs_proj/sfs_plots/'
     paths_helper = get_paths_helper(options.dataset_name)
     if not options.args:

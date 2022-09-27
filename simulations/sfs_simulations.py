fefa12dbbbcab582d1af51a4900500b85f38f0a6
Shahar <shahar.mazie@mail.huji.ac.il>
2022-09-27 19:32:06 +0300

    a

--- simulations/sfs_simulations.py
+++ simulations/sfs_simulations.py
@@ -89,7 +89,7 @@ def sfs2R(sfs, hot_spot):
 
 def plot_by_generations(options, plots_base_dir, migration_rate):
     pop_sizes = np.array([8, 12])
-    iterations = 100
+    iterations = 5
     gens = np.arange(20) ** 2 + 1
     hot_spot = np.min(pop_sizes) * 2
     gens2R_mean = np.empty(shape=gens.size)

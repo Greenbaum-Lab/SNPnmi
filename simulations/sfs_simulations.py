e1b94b743e258ea6885585f19c736b6f42dd327f
Shahar <shahar.mazie@mail.huji.ac.il>
2022-09-28 23:18:31 +0300

    a

--- simulations/sfs_simulations.py
+++ simulations/sfs_simulations.py
@@ -123,7 +123,7 @@ def plot_by_generations(options, plots_base_dir, migration_rate, single_plot=Fal
 
 
 def submit_all_migration_rates(options, paths_helper):
-    m_rates = (np.arange(100) + 1) / (10 ** 4)
+    m_rates = (np.arange(100) + 1) / (10 ** 5)
     job_type = 'simulations_job'
     script_path = os.path.abspath(__file__)
     errs = []
@@ -148,7 +148,7 @@ def submit_all_migration_rates(options, paths_helper):
 if __name__ == '__main__':
     options = args_parser()
     options.dataset_name = 'simulations'
-    plots_base_dir = '/sci/labs/gilig/shahar.mazie/icore-data/sfs_proj/sfs_plots2/'
+    plots_base_dir = '/sci/labs/gilig/shahar.mazie/icore-data/sfs_proj/sfs_plots/'
     paths_helper = get_paths_helper(options.dataset_name)
     if not options.args:
         os.makedirs(plots_base_dir, exist_ok=True)

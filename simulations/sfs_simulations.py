4271af884bc737b82c7d0357278cb08d02779bdd
Shahar <shahar.mazie@mail.huji.ac.il>
2022-10-15 14:39:16 +0300

    submit simulations jobs

--- simulations/sfs_simulations.py
+++ simulations/sfs_simulations.py
@@ -235,9 +235,9 @@ def combine_migration_json2heatmap(plots_base_dir):
     ttl = ax.title
     ttl.set_position([0.5, 1.02])
     ax.set_xticks(GENERATIONS)
-    s = sns.heatmap(peak_scores, fmt="", cmap='RdYlGn', ax=ax, xticklabels=GENERATIONS,
+    s = sns.heatmap(peak_scores, cmap='RdYlGn', ax=ax, xticklabels=GENERATIONS,
                     yticklabels=M_RATES, cbar_kws={"ticks": np.arange(int(np.nanmax(peak_scores))) + 1,
-                                                   'label': 'Peak score'})
+                                                   "label": 'Peak score'})
     plt.locator_params(axis='y', nbins=20)
     s.set_xlabel('Generations', fontsize=16)
     s.set_ylabel('Migration rate', fontsize=16)

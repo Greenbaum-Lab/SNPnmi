a1a7e8dd4a9ae1c9070b241021fa6b08d7f98646
Shahar <shahar.mazie@mail.huji.ac.il>
2022-10-26 12:29:21 +0300

    submit simulations jobs

--- simulations/sfs_simulations.py
+++ simulations/sfs_simulations.py
@@ -328,8 +328,8 @@ def manage_sample_size_runs(options, paths_helper, base_dir):
     os.makedirs(output_dir, exist_ok=True)
     if not options.args:
         os.makedirs(output_dir, exist_ok=True)
-        # submit_all_sample_sizes(options, paths_helper, output_dir)
-        # combine_sample_size2heatmap(output_dir)
+        submit_all_sample_sizes(options, paths_helper, output_dir)
+        combine_sample_size2heatmap(output_dir)
         combine_json2sample_size_plot(output_dir)
     else:
         p1 = int(options.args[0])

5a7f4ef6623a22efd3a738a3459db01306b3f4b7
Shahar <shahar.mazie@mail.huji.ac.il>
2022-11-10 16:18:48 +0200

    General Heatmap plots

--- utils/s7_join_to_summary/plots_helper.py
+++ utils/s7_join_to_summary/plots_helper.py
@@ -96,7 +96,7 @@ def plot_per_class(options, mac_maf, values, std, scats, polynomials, base_lines
 def heatmap_plot(output, data_matrix, x_label, y_label, x_ticks, y_ticks, xticks_labels, yticks_labels, c_bar_label,
                  title, y_bins=False, x_bins=False):
     fig, ax = plt.subplots(figsize=(12, 8))
-    plt.title(title, fontsize=18)
+    plt.title(title + "sdfgsdfgdfG", fontsize=18)
     ttl = ax.title
     ttl.set_position([0.5, 1.02])
     s = sns.heatmap(data_matrix, cmap='RdYlGn', ax=ax,

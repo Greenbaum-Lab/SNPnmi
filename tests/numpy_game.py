ecd0cfa6a1395dbc34a48f6d6604244980ede9b6
Shahar <shahar.mazie@mail.huji.ac.il>
2022-11-14 23:18:09 +0200

    General Heatmap plots

--- tests/numpy_game.py
+++ tests/numpy_game.py
@@ -3,3 +3,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 from sfs_analysis.sfs_utils import get_ticks_locations
+
+a = (np.random.random(30) * 2 + np.arange(30) ** 0.5) / 10
+plt.plot(a)
+plt.fill_between(x=np.arange(30),y1=a - .1, y2=a+.1, alpha=0.3)
+plt.xlabel("MAC")
+plt.ylabel("NMI score")
+plt.show()
+enumerate()

f94c9b195ef44acf118e769462b215059b57e3da
Shahar <shahar.mazie@mail.huji.ac.il>
2022-11-26 23:34:54 +0200

    add violin plot

--- sfs_analysis/split_dataset_by_subpops.py
+++ sfs_analysis/split_dataset_by_subpops.py
@@ -319,7 +319,7 @@ def main():
 
     # submit_all_sites(arguments, paths_helper, run_step)
     # create_heat_map(arguments, paths_helper, sites_list)
-    compare_heatmap_to_fst(arguments, paths_helper, 'hgdp_fst.txt')
+    # compare_heatmap_to_fst(arguments, paths_helper, 'hgdp_fst.txt')
     violin_plot(paths_helper)
 
 

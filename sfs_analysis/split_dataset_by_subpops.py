e32e09d491226c8df11d90cd15081219936ecadb
Shahar <shahar.mazie@mail.huji.ac.il>
2022-11-26 23:45:16 +0200

    add violin plot

--- sfs_analysis/split_dataset_by_subpops.py
+++ sfs_analysis/split_dataset_by_subpops.py
@@ -286,9 +286,13 @@ def violin_plot(paths_helper):
     within_regions = []
     across_regions = []
     heat_map_df = pd.read_csv(f'{paths_helper.sfs_dir_chr}summary/relative_heat.csv')
-    for pop1, row in heat_map_df.iterrows():
-        for pop2, val in row.items():
-            if continents_dict[pop1] == continents_dict[pop2]:
+    pops = list(heat_map_df.iloc[0].index)[1:]
+    for _, row in heat_map_df.iterrows():
+        pop1 = row[0]
+        for idx, val in enumerate(row[1:]):
+            if pops[idx] == pop1:
+                continue
+            if continents_dict[pop1] == continents_dict[pops[idx]]:
                 within_regions.append(val)
             else:
                 across_regions.append(val)
